8b93cd03295bbd13390a81f5f9a7de5babcc211c
bikramia <bikram.rhce@gmail.com>
2025-03-16 16:40:57 -0400

    Arrays -changing values

--- DataStructure/src/com/bikram/Main.java
+++ DataStructure/src/com/bikram/Main.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] names) {
         int[] numbers = new int[3];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
         System.out.println(Arrays.toString(numbers));
     }
 }
