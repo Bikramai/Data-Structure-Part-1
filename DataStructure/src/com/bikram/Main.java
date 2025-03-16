4ecdcacf6e35c237a29e31609cdb188623d53b7d
bikramia <bikram.rhce@gmail.com>
2025-03-16 16:48:33 -0400

    Arrays -Size fo items 5, and we cannot change it, so if you want to store 6 th items here.

--- DataStructure/src/com/bikram/Main.java
+++ DataStructure/src/com/bikram/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] names) {
         int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println(numbers.length);
         System.out.println(Arrays.toString(numbers));
     }
 }
