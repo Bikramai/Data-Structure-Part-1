6e7f09513842c7799d57b90f312cea4355c11659
bikramia <bikram.rhce@gmail.com>
2025-03-16 16:38:52 -0400

    Arrays

---
+++ DataStructure/src/com/bikram/Main.java
@@ -0,0 +1,11 @@
+package com.bikram;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] names) {
+        int[] numbers = new int[3];
+        System.out.println(Arrays.toString(numbers));
+    }
+}
+
