1dc006b6134f74025bc42e936332653b8a40fa5a
bikramia <bikram.rhce@gmail.com>
2025-03-18 14:12:06 -0400

    ExeArray Dynamic arrays

--- DataStructure/src/com/bikram/Main.java
+++ DataStructure/src/com/bikram/Main.java
@@ -4,8 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] names) {
-        Array numbers = new Array(3);
+        Array numbers = new Array(5);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(50);
+        numbers.insert(60);
+        numbers.insert(70);
+        numbers.removeAt(6);
         numbers.print();
+        System.out.println(numbers.indexOf(10));
     }
 }
 
