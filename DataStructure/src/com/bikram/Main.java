b74952fe8c91a32af4bb79dc19829cadf61c7332
bikramia <bikram.rhce@gmail.com>
2025-03-21 09:52:31 -0400

    Space Complexity - o(1)

--- DataStructure/src/com/bikram/Main.java
+++ DataStructure/src/com/bikram/Main.java
@@ -1,23 +1,9 @@
 package com.bikram;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] names) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        list.remove(0);
-        list.indexOf(20);
-        list.lastIndexOf(20);
-        list.contains(20);
-        list.size();
-        list.toArray();
-        System.out.println(list);
+        // O(1) space
+        for (int i = 0; i < names.length; i++)
+            System.out.println("Hi " + names[i]);
     }
 }
 
