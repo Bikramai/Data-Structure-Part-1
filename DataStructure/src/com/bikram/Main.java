1e75fce032a5c5c5acd96dd6bfbd3b18c716bc6d
bikramia <bikram.rhce@gmail.com>
2025-03-21 09:59:07 -0400

    Space Complexity - o(n)

--- DataStructure/src/com/bikram/Main.java
+++ DataStructure/src/com/bikram/Main.java
@@ -2,6 +2,7 @@ package com.bikram;
 public class Main {
     public static void main(String[] names) {
         // O(1) space
+        String[] copy = new String[names.length];
         for (int i = 0; i < names.length; i++)
             System.out.println("Hi " + names[i]);
     }
